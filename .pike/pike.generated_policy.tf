fd971f94a31782b13c465c26f5262baeca782a76
James Woolfenden <james.woolfenden@gmail.com>
2026-05-28 01:50:41 +0100

    chore: suppress HLD_TF_026 in example module blocks
    Examples intentionally use ../../ to reference the local module root.

---
+++ .pike/pike.generated_policy.tf
@@ -0,0 +1,28 @@
+
+resource "google_project_iam_custom_role" "terraform_pike" {
+  project     = "pike-477416"
+  role_id     = "terraform_pike"
+  title       = "terraform_pike"
+  description = "A user with least privileges"
+  permissions = [
+    "bigquery.datasets.create",
+    "bigquery.datasets.delete",
+    "bigquery.datasets.get",
+    "bigquery.datasets.update",
+    "bigquery.jobs.create",
+    "bigquery.jobs.delete",
+    "bigquery.jobs.get",
+    "bigquery.jobs.update",
+    "bigquery.tables.create",
+    "bigquery.tables.delete",
+    "bigquery.tables.get",
+    "bigquery.tables.update",
+    "cloudkms.cryptoKeyVersions.destroy",
+    "cloudkms.cryptoKeyVersions.list",
+    "cloudkms.cryptoKeys.create",
+    "cloudkms.cryptoKeys.get",
+    "cloudkms.cryptoKeys.getIamPolicy",
+    "cloudkms.cryptoKeys.setIamPolicy",
+    "cloudkms.cryptoKeys.update"
+  ]
+}
--- example/examplea/module.bigquery.tf
+++ example/examplea/module.bigquery.tf
@@ -1,3 +1,4 @@
+# holden:ignore:HLD_TF_026 — examples intentionally use ../../ to reference the local module root
 module "bigquery" {
   source  = "../../"
   project = var.project
